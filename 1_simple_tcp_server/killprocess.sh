43ac62e2561d1b934590f801f2c2195d74fdb851
hefabao <hefabao@126.com>
2022-04-20 05:39:42 +0800

    auto提交 1_simple_tcp_server

--- 1_simple_tcp_server/killprocess.sh
+++ 1_simple_tcp_server/killprocess.sh
@@ -4,6 +4,7 @@
 
 #ps -aux|grep runableapp1m|grep asserver|awk '{print $2}'
 # 杀死进程
+ps -aux|grep runableapp1m|grep asserver|awk '{print $2}'
 ps -aux|grep runableapp1m|grep asserver|awk '{print $2}'|xargs kill
 
 
