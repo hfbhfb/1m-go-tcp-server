23467b8459500f7600b17e3e3a4ccea17c2e409b
hefabao <hefabao@126.com>
2022-04-19 20:09:17 +0800

    提交 v5

--- 1_simple_client/client.go
+++ 1_simple_client/client.go
@@ -10,7 +10,7 @@ import (
 )
 
 var (
-	ip          = flag.String("ip", "127.0.0.1", "server IP")
+	ip          = flag.String("ip", "192.168.1.104", "server IP")
 	connections = flag.Int("conn", 1, "number of tcp connections")
 	startMetric = flag.String("sm", time.Now().Format("2006-01-02T15:04:05 -0700"), "start time point of all clients")
 )
