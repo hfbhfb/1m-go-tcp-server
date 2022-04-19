0430f8314129ebb4747b843ead94b1dcfbe8e711
hefabao <hefabao@126.com>
2022-04-20 05:33:26 +0800

    auto提交 1_simple_tcp_server

--- 1_simple_tcp_server/runprocess.sh
+++ 1_simple_tcp_server/runprocess.sh
@@ -4,8 +4,6 @@
 
 #ps -aux|grep runableapp1m|grep asserver|awk '{print $2}'
 # 启动进程
-cd /home/usera/projects/go/mod-pro/1m-go-tcp-server/1_simple_tcp_server
-./runableapp1m asserver &
-
+./runableapp1m  &
 
 
