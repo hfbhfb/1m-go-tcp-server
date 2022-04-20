e970650c9b4ca4b9e1bc92e49bc459bd0603ec4d
hefabao <hefabao@126.com>
2022-04-20 13:23:57 +0800

    auto提交 1_simple_tcp_server

--- 1_simple_client/killprocess.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-
-# 杀死进程
-# ps -aux|grep runableapp1m|grep asclient|awk '{print $2}'|xargs kill
-
-
-
--- 1_simple_client/runprocess.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-
-#ps -aux|grep runableapp1m|grep asclient|awk '{print $2}'
-# 启动进程
-nohup ./runableapp1m asclient &
-
-# nohup ./runableapp1m asclient &
