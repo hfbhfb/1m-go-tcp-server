df2ae837f735c2a9d2f966c026c766f315a04ae8
hefabao <hefabao@126.com>
2022-04-20 12:53:37 +0800

    auto提交 1_simple_client

--- 1_simple_client/runprocess.sh
+++ 1_simple_client/runprocess.sh
@@ -2,6 +2,8 @@
 
 
 
-#ps -aux|grep runableapp1m|grep asserver|awk '{print $2}'
+#ps -aux|grep runableapp1m|grep asclient|awk '{print $2}'
 # 启动进程
 nohup ./runableapp1m asclient &
+
+# nohup ./runableapp1m asclient &
